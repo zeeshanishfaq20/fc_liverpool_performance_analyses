e6a4b43daf51fcd5c0b80bbb7d8f836445caebf2
zeeshanishfaq20 <zeeshan20@gmail.com>
2025-03-25 21:26:09 +0500

    updated files

--- models/marts/core/dim__player_each_match_performance.sql
+++ models/marts/core/dim__player_each_match_performance.sql
@@ -18,35 +18,39 @@ with player_stats as (
         coalesce(yellow_cards, 0) as yellow_cards,
         coalesce(yellow_red_cards, 0) as yellow_red_cards
     from {{ ref('int__sport_event_summary') }}
+),
+
+aggregated_player as (
+    select distinct
+        event_id,
+        player_name,
+        competitor_name,
+        competition_name,
+        season_name,
+        date(start_time) as match_date,
+        count(match_date) over (partition by player_name, competitor_name, competition_name) as no_of_matches_played,
+        assists,
+        passes_successful,
+        shots_on_target,
+        minutes_played,        
+        goals_scored,
+        offsides,
+        own_goals,
+        red_cards,
+        shots_blocked,
+        yellow_cards,
+        sum(assists) over (partition by player_name, competitor_name, competition_name) as total_assists,
+        sum(passes_successful) over (partition by player_name, competitor_name, competition_name) as total_passes_successful,
+        sum(shots_on_target) over (partition by player_name, competitor_name, competition_name) as total_shots_on_target,
+        sum(minutes_played) over (partition by player_name, competitor_name, competition_name) as total_minutes_played,
+        sum(goals_scored) over (partition by player_name, competitor_name, competition_name) as total_goals_scored,
+        sum(offsides) over (partition by player_name, competitor_name, competition_name) as total_offsides,
+        sum(own_goals) over (partition by player_name, competitor_name, competition_name) as total_own_goals,
+        sum(red_cards) over (partition by player_name, competitor_name, competition_name) as total_red_cards,
+        sum(shots_blocked) over (partition by player_name, competitor_name, competition_name) as total_shots_blocked,
+        sum(yellow_cards) over (partition by player_name, competitor_name, competition_name) as total_yellow_cards
+    from player_stats
+    order by competitor_name, competition_name
 )
 
-select distinct
-    event_id,
-    player_name,
-    competitor_name,
-    competition_name,
-    season_name,
-    date(start_time) as match_date,
-    count(match_date) over (partition by player_name, competitor_name, competition_name) as no_of_matches_played,
-    assists,
-    passes_successful,
-    shots_on_target,
-    minutes_played,        
-    goals_scored,
-    offsides,
-    own_goals,
-    red_cards,
-    shots_blocked,
-    yellow_cards,
-    sum(assists) over (partition by player_name, competitor_name, competition_name) as total_assists,
-    sum(passes_successful) over (partition by player_name, competitor_name, competition_name) as total_passes_successful,
-    sum(shots_on_target) over (partition by player_name, competitor_name, competition_name) as total_shots_on_target,
-    sum(minutes_played) over (partition by player_name, competitor_name, competition_name) as total_minutes_played,
-    sum(goals_scored) over (partition by player_name, competitor_name, competition_name) as total_goals_scored,
-    sum(offsides) over (partition by player_name, competitor_name, competition_name) as total_offsides,
-    sum(own_goals) over (partition by player_name, competitor_name, competition_name) as total_own_goals,
-    sum(red_cards) over (partition by player_name, competitor_name, competition_name) as total_red_cards,
-    sum(shots_blocked) over (partition by player_name, competitor_name, competition_name) as total_shots_blocked,
-    sum(yellow_cards) over (partition by player_name, competitor_name, competition_name) as total_yellow_cards
-from player_stats
-order by competitor_name, competition_name
+select * from aggregated_player
--- tests/test__player_stats_consistency.sql
+++ tests/test__player_stats_consistency.sql
@@ -1,27 +1,11 @@
 with invalid_data as (
-    select 
-        assists,
-        passes_successful,
-        shots_on_target,
-        minutes_played,        
+    select
+        player_name,
         goals_scored,
-        offsides,
-        own_goals,
-        red_cards,
-        shots_blocked,
-        yellow_cards
+        total_goals_scored
     from {{ ref('dim__player_each_match_performance') }}
     where 
-        total_assists >= assists  and 
-        total_passes_successful >= passes_successful  and
-        total_shots_on_target >= shots_on_target  and
-        total_minutes_played >= minutes_played  and
-        total_goals_scored >= goals_scored  and
-        total_offsides >= offsides  and
-        total_own_goals >= own_goals  and
-        total_red_cards >= red_cards  and
-        total_shots_blocked >= shots_blocked  and
-        total_yellow_cards >= yellow_cards 
+        total_goals_scored >= goals_scored 
 )
 select * 
 from invalid_data
